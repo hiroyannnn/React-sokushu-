d5d989087f157357643b275409289fee2b908e81
hiro <hiro@hironoMacBook-puro.local>
2019-03-31 19:04:01 +0900

    関数化

--- quick-react/src/MyArticle.js
+++ quick-react/src/MyArticle.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 import MyNew from './MyNew';
 
 export default class MyArticle extends Component {
+    // 条件分岐によるコード
+    renderIfNew(isNew) {
+        if (isNew) {
+            return <MyNew />
+        }
+    }
+
     render() {
         return (
             <React.Fragment>
@@ -10,11 +17,12 @@ export default class MyArticle extends Component {
                         {this.props.title}</a>
                     {/* {this.props.isNew ? <MyNew /> : null} */}
                     {/* {this.props.isNew && <MyNew />} */}
-                    {(() => {
+                    {/* {(() => {
                         if (this.props.isNew) {
                             return <MyNew />
                         }
-                    })()}
+                    })()} */}
+                    {this.renderIfNew(this.props.isNew)}
                 </dt>
                 <dd>
                     {this.props.description}
