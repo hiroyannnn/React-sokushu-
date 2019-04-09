3558f6dac3fa96d3cd8f717869e005f13651b5c6
hiro <hiro@hironoMacBook-puro.local>
2019-04-09 22:20:20 +0900

    mySelect

--- quick-react/src/index.js
+++ quick-react/src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
 import * as serviceWorker from './serviceWorker';
-import MyTextarea from './MyTextarea';
+import MySelect from './MySelect';
+// import MyTextarea from './MyTextarea';
 
 // import MyForm from './MyForm';
 // import MyHello from './MyHello';
@@ -120,8 +121,11 @@ ReactDOM.render(
     // form
     // <MyForm />,
 
-    // form
-    <MyTextarea />,
+    // form　textarea
+    // <MyTextarea />,
+
+    // from select
+    <MySelect />,
     document.getElementById('root')
 );
 
